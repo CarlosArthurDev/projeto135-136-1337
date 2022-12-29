5f1ab70fc28d645e9dbf2ff14a199925e437cb18
CarlosArthurDev <103152440+CarlosArthurDev@users.noreply.github.com>
2022-12-29 18:38:18 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 var video=""
 var modelStatus=""
+objects=[]
+
+
 function preload(){
     video=createVideo("video.mp4")
    
@@ -11,6 +14,21 @@ function setup(){
 }
 function draw(){
     image(video,0,0,480,380)
+
+    if(modelStatus!=""){
+        objectDetector.detect(video,gotResult)
+        for(i=0;i<objects.length;i++){
+            document.getElementById("status").innerHTML="status: objetos detectados"
+            document.getElementById("numberOfObjects").innerHTML="quantitade de objetos detectados: "+objects.length
+            fill("#FF0000")
+            percent=floor(objects[i].confidence*100)
+            text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15)
+            noFill()
+            stroke("#FF0000")
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height)
+        }
+    }
+    
 }
 function start(){
     objectDetector=ml5.objectDetector("cocossd",modelLoaded)
@@ -22,4 +40,11 @@ function modelLoaded(){
     video.loop()
     video.speed(1)
     video.volume(0)
+}
+function gotResult(error,results){
+    if(error){
+        console.log(error)
+    }
+    console.log(results)
+    objects=results
 }
